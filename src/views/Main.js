3b2d7729e7101ce0929a020d9491d75b2ebc07a7
Daniel <danix100@hotmail.com>
2020-06-06 15:33:39 -0500

    Google Sign In - Data collection

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,26 +1,34 @@
 import m from "mithril";
 
+
 function Main() {
-    var render = false;
-    return {
-        view: function(){
-            return m("div",[
+    var render = false
+    var GoogleAuth
+    return {        
+        oninit: function() {
+            gapi.load('auth2', function() {
+                gapi.auth2.init({
+                    client_id: '325821250313-rpe75eqduheb1dduudjdq4cbhb72fro4.apps.googleusercontent.com'
+                }).then(function(response){
+                    console.log("ya cargo el api we")
+                    GoogleAuth = response
+                    GoogleAuth.currentUser.listen(function(GoogleUser) {
+                        console.log(GoogleUser)
+                    })
+                })
+            });
+        },
+        view: function () {
+            return m("div", [
                 m("div", "Eh we, picale we"),
-                m("button",{
+                m("button", {
                     onclick: () => {
                         render = !render
                     }
-                },"ACA WE"),
-                render ? m("div","tas bien meco we, me hiciste caso xdxd") : "",
-                m("div",{
-                    class: "g-signin2",
-                    "data-onsuccess": function(googleUser) {
-                        var profile = googleUser.getBasicProfile();
-                        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-                        console.log('Name: ' + profile.getName());
-                        console.log('Image URL: ' + profile.getImageUrl());
-                        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-                    }
+                }, "ACA WE"),
+                render ? m("div", "tas bien meco we, me hiciste caso xdxd") : "",
+                m("div", {
+                    class: "g-signin2"
                 })
             ])
         }
