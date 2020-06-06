ee804d5ba6c031998cdd66936ad4bfae3a00a225
Daniel <danix100@hotmail.com>
2020-06-05 19:53:43 -0500

    Test with google sign in

--- src/views/Main.js
+++ src/views/Main.js
@@ -11,7 +11,17 @@ function Main() {
                         render = !render
                     }
                 },"ACA WE"),
-                render && m("tas bien meco we, me hiciste caso xdxd")
+                render ? m("div","tas bien meco we, me hiciste caso xdxd") : "",
+                m("div",{
+                    class: "g-signin2",
+                    "data-onsuccess": function(googleUser) {
+                        var profile = googleUser.getBasicProfile();
+                        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+                        console.log('Name: ' + profile.getName());
+                        console.log('Image URL: ' + profile.getImageUrl());
+                        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+                    }
+                })
             ])
         }
     }
