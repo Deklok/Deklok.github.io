00bc75add1edc1c37f3353fc46e4c707a67ac191
Daniel <danix100@hotmail.com>
2020-06-07 17:27:41 -0500

    Point to https (need workaround for cert)

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 import m from "mithril";
-var BASE_URL = "http://ec2-54-173-20-147.compute-1.amazonaws.com:8080";
+var BASE_URL = "https://ec2-54-173-20-147.compute-1.amazonaws.com:8080";
 //var BASE_URL = "https://localhost:8080";
 
 var User = {
