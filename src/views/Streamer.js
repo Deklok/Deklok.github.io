1117faac3cbfe7b780d0f418027115393aabccee
Daniel <danix100@hotmail.com>
2020-06-08 18:09:49 -0500

    First screen

--- src/views/Streamer.js
+++ src/views/Streamer.js
@@ -1,50 +1,81 @@
 import m from "mithril";
 
 function Streamer() {
+    var livesession = false;
     return {
         view: function(vnode) {
             return m("div",{
-                class: "card-panel #8c9eff indigo accent-1"
+                class: "card-panel purple darken-4",
+                style: "border-radius: 10px;"
             },[
-                m("div",[
-                    m("div",{
-                        class: "waves-effect waves-light btn white"
+                m("div",{
+                    style: "margin-bottom: 10%; text-align: left"
+                },[
+                    m("button",{
+                        class: "waves-effect waves-purple btn white",
+                        onclick: () => {
+                            var badge = document.getElementById("livebadge").classList;
+                            if (livesession) {
+                                badge.remove("red","darken-4");
+                                badge.add("purple","lighten-3");
+                            } else {
+                                badge.remove("purple","lighten-3");
+                                badge.add("red","darken-4");
+                            }
+                            livesession = !livesession;
+                        }
                     },[
                         m("div",{
                             class: "black-text"
                         },"Iniciar sesion en tu gfa")
+                    ]),
+                    m("span",{
+                        id: "livebadge",
+                        class: "badge purple lighten-3"
+                    },[
+                        m("div",{
+                            class: "white-text"
+                        },"LIVE")
                     ])
                 ]),
-                m("div",[
-                    m("div",{
-                        class: "waves-effect waves-light btn white"
+                m("div",{
+                    class: "btn-streamer-main"
+                },[
+                    m("button",{
+                        class: "waves-effect waves-purple btn white"
                     },[
                         m("div",{
                             class: "black-text"
                         },"Pantalla")
                     ])
                 ]),
-                m("div",[
-                    m("div",{
-                        class: "waves-effect waves-light btn white"
+                m("div",{
+                    class: "btn-streamer-main"
+                },[
+                    m("button",{
+                        class: "waves-effect waves-purple btn white"
                     },[
                         m("div",{
                             class: "black-text"
                         },"Moderación")
                     ])
                 ]),
-                m("div",[
-                    m("div",{
-                        class: "waves-effect waves-light btn white"
+                m("div",{
+                    class: "btn-streamer-main"
+                },[
+                    m("button",{
+                        class: "waves-effect waves-purple btn white"
                     },[
                         m("div",{
                             class: "black-text"
                         },"Configuración")
                     ])
                 ]),
-                m("div",[
-                    m("div",{
-                        class: "waves-effect waves-light btn white"
+                m("div",{
+                    style: "margin-top: 20%;text-align: right;"
+                },[
+                    m("button",{
+                        class: "waves-effect waves-purple btn white",
                     },[
                         m("div",{
                             class: "black-text"
